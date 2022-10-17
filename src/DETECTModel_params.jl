6273cdcd97d0579cac24e0277b1f57eb4957bc7a
AlexisRenchon <a.renchon@gmail.com>
2022-10-17 14:44:07 -0400

    example params

---
+++ src/DETECTModel_params.jl
@@ -0,0 +1,27 @@
+# example of parameter value, from https://gmd.copernicus.org/articles/11/1909/2018/#&gid=1&pid=1
+
+Rᶜ = FT(111.5) # mg C cm-2, nb: do we use unitful?
+Rᵦ = FT(6e-5)
+α₁ᵣ = FT(11.65)
+α₂ᵣ = FT(20.7)
+α₃ᵣ = FT(-164.2)
+Sᶜ = FT(711.6)
+Mᶜ = FT(12.3)
+Vᵦ = FT(0.0015)
+α₁ₘ = FT(14.05)
+α₂ₘ = FT(11.05)
+α₃ₘ = FT(-87.6)
+Kₘ = FT(10e-5)
+CUE = FT(0.8)
+p = FT(0.004)
+Dₗᵢ = FT(3.17)
+E₀ₛ = FT(324.6) 
+T₀ = FT(227.5)
+α₄ = FT(4.7)
+α₅ = FT(4.547)
+BD = FT(1.12)
+ϕ₁₀₀ = FT(18.46)
+PD = FT()
+
+
+
