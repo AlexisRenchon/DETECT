c598e0f7b2dea211bbf0ecbc778fc86ecaabc004
AlexisRenchon <a.renchon@gmail.com>
2022-10-17 11:28:53 -0400

    auxiliary

--- src/DETECTModel_auxiliary.jl
+++ src/DETECTModel_auxiliary.jl
@@ -1,6 +1,14 @@
+# define functions for DETECT auxiliary variables
 
-
-
+CO2_diffusivity()
+diffusion_coefficient()
+soil_porosity()
+Root_source()
+root_temp_θ_adj1()
+root_temp_θ_adj2()
+Microbe_source()
+Vmax()
+Csol()
 
 
 
